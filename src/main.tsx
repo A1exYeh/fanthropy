a32826eadb95bbf59eb15034aa93c68b55079876
Alex Yeh <111445339+AlexYessirski@users.noreply.github.com>
2025-01-05 16:51:15 -0800

    setup gh-pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import LoginPage from './pages/LoginPage/LoginPage';
 import TeamsPage from './pages/TeamsPage/TeamsPage';
@@ -9,13 +9,13 @@ import ThreeSquare from './pages/Organizations/ThreeSquare/ThreeSquare';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <Router basename='/fanthropy'>
     <Routes>
       <Route index element={<LoginPage />} />
       <Route path='/teams' element={<TeamsPage />} />
       <Route path='/orgs' element={<OrgsPage />} />
       <Route path='/orgs/threesquare' element={<ThreeSquare/>} />
     </Routes>
-    </BrowserRouter>
+    </Router>
   </StrictMode>
 );
