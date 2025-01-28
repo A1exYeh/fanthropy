aa3dce4ed85528af869445a81a949caf4843da3a
Alex Yeh <111445339+AlexYessirski@users.noreply.github.com>
2025-01-28 14:13:15 -0800

    delete old files

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { BrowserRouter } from 'react-router';
-import { createRoot } from 'react-dom/client';
-import { App } from './app';
-
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename='/fanthropy'>
-    <App />
-  </BrowserRouter>
-);
