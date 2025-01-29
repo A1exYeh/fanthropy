574d4ed02ba4800431396dc9fb75742e587d9851
Alex Yeh <111445339+AlexYessirski@users.noreply.github.com>
2025-01-29 11:54:06 -0800

    change navbar size

--- fanthropy/src/components/navbar.tsx
+++ fanthropy/src/components/navbar.tsx
@@ -4,7 +4,7 @@ export const Navbar = () => {
   return (
     <>
       <div className="w-full flex justify-center items-center bg-primary/35 backdrop-blur-sm drop-shadow-xs fixed top-0 z-10">
-        <div className="w-full max-w-screen-xl flex flex-row items-center justify-end gap-4 p-4">
+        <div className="w-full text-sm max-w-screen-lg flex flex-row items-center justify-end gap-6 p-3">
           <a href="/" className="mr-auto">
             <img src={ViteLogo} alt="Logo" />
           </a>
